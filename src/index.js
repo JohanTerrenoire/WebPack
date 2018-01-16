15899d64370a962c3120877f64a63bbfb2a6f251
JohanTerrenoire <terrenoirejohan@yahoo.fr>
2018-01-16 08:39:09 +0100

    Premier Commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function test(){
+  console.log("test");
+  document.write("La fonction test() a été éxécutée");
+}
+
+document.write("<h1>Hello World !</h1></br>");
+document.write("<p>WebPack est bien installé.</p>")
+test();
